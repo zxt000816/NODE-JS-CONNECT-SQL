ac6445d43b40480c95a9eb4661d3334ccae9e1ee
zyf0706 <zyf13815628123@gmail.com>
2023-03-02 11:17:28 +0900

    deleted:    CreateTestData.sql 	modified:   README.md 	deleted:    columnstore.js 	modified:   db.js 	modified:   package-lock.json 	modified:   package.json 	CreateForwardTransaction.sql 	utils.js

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=zyf0706@." -p 1433:1433 -d mcr.microsoft.com/mssql/server:latest
 
 
-docker exec -it 1500e75afd72 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P zyf0706@.
+docker exec -it fa07e241c5bb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P zyf0706@.
 
 
-docker exec -it 1500e75afd72 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P zyf0706@. -d SampleDB -i ./CreateTestData.sql
+docker exec -it fa07e241c5bb /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P zyf0706@. -d SampleDB -i ./CreateTestData.sql
--- db.js
+++ db.js
@@ -1,157 +1,101 @@
-var Connection = require('tedious').Connection;
-var Request = require('tedious').Request;
-var TYPES = require('tedious').TYPES;
-var async = require('async');
+const sql = require('mssql');
+const { generateRandomObject, parseData } = require('./utils.js');
 
-// Create connection to database
-var config = {
-    server: 'localhost',
-    authentication: {
-        type: 'default',
-        options: {
-            userName: 'sa', // update me
-            password: 'zyf0706@.' // update me
-        }
-    },
+const config = {
+    user: 'sa',
+    password: 'zyf0706@.',
+    server: "localhost",
+    database: 'yifan_test',
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+      },
     options: {
-        encrypt: true,
-        trustServerCertificate: true,
-        database: 'SampleDB'
+      encrypt: true,
+      trustServerCertificate: true,
     }
 }
-var connection = new Connection(config);
 
-connection.connect();
+async function InsertOneMsSQL(data) {
+    await sql.connect(config);
+    console.log("connected");
 
-function Start(callback) {
-    console.log('Starting...');
-    callback(null, 'Jake', 'United States');
+    const result = await sql.query`INSERT INTO ForwardTransaction.Contract (buyName, buyBirth, buyAddress, buyPhone, buySubPhone, sellerName, \
+        sellerBirth, sellerAddress, sellerPhone, sellerSubPhone, item, kind, areaFlat, formalDay, totalPrice, firstYn, \
+        firstPrice, firstEndCount, secondYn, secondPrice, secondEndCount, thirdYn, thirdPrice, thirdEndCount, etc, returnDate) \
+        VALUES (${data.buyName}, ${data.buyBirth}, ${data.buyAddress}, ${data.buyPhone}, ${data.buySubPhone}, \
+        ${data.sellerName}, ${data.sellerBirth}, ${data.sellerAddress}, ${data.sellerPhone}, ${data.sellerSubPhone}, \
+        ${data.item}, ${data.kind}, ${data.areaFlat}, ${data.formalDay}, ${data.totalPrice}, ${data.firstYn}, ${data.firstPrice}, \
+        ${data.firstEndCount}, ${data.secondYn}, ${data.secondPrice}, ${data.secondEndCount}, ${data.thirdYn}, ${data.thirdPrice}, \
+        ${data.thirdEndCount}, ${data.etc}, ${data.returnDate})`;
+
+    console.dir(result);
+    console.log("inserted");
+
+    sql.close();
 }
 
-function Insert(name, location, callback) {
-    console.log("Inserting '" + name + "' into Table...");
+async function SearchOneMsSQL(id) {
+    await sql.connect(config);
+    console.log("connected");
 
-    request = new Request(
-        'INSERT INTO TestSchema.Employees (Name, Location) OUTPUT INSERTED.Id VALUES (@Name, @Location);',
-        function(err, rowCount, rows) {
-        if (err) {
-            callback(err);
-        } else {
-            console.log(rowCount + ' row(s) inserted');
-            callback(null, 'Nikita', 'United States');
-        }
-        });
-    request.addParameter('Name', TYPES.NVarChar, name);
-    request.addParameter('Location', TYPES.NVarChar, location);
+    const result = await sql.query`SELECT * FROM ForwardTransaction.Contract WHERE Id = ${id}`;
 
-    // Execute SQL statement
-    connection.execSql(request);
+    console.log(result);
+    console.log("searched");
+
+    sql.close();
 }
 
-function Update(name, location, callback) {
-    console.log("Updating Location to '" + location + "' for '" + name + "'...");
+async function SearchAllMsSQL() {
+    await sql.connect(config);
+    console.log("connected");
 
-    // Update the employee record requested
-    request = new Request(
-    'UPDATE TestSchema.Employees SET Location=@Location WHERE Name = @Name;',
-    function(err, rowCount, rows) {
-        if (err) {
-        callback(err);
-        } else {
-        console.log(rowCount + ' row(s) updated');
-        callback(null, 'Jared');
-        }
-    });
-    request.addParameter('Name', TYPES.NVarChar, name);
-    request.addParameter('Location', TYPES.NVarChar, location);
+    const result = await sql.query`SELECT * FROM ForwardTransaction.Contract`;
+    
+    console.log(result);
+    console.log("searched");
 
-    // Execute SQL statement
-    connection.execSql(request);
+    sql.close();
 }
 
-function Delete(name, callback) {
-    console.log("Deleting '" + name + "' from Table...");
+async function DeleteOneMsSQL(id) {
+    await sql.connect(config);
+    console.log("connected");
 
-    // Delete the employee record requested
-    request = new Request(
-        'DELETE FROM TestSchema.Employees WHERE Name = @Name;',
-        function(err, rowCount, rows) {
-        if (err) {
-            callback(err);
-        } else {
-            console.log(rowCount + ' row(s) deleted');
-            callback(null);
-        }
-        });
-    request.addParameter('Name', TYPES.NVarChar, name);
+    await sql.query`DELETE FROM ForwardTransaction.Contract WHERE id = ${id}`;
 
-    // Execute SQL statement
-    connection.execSql(request);
+    console.log("deleted");
+
+    sql.close();
 }
 
-function Read(callback) {
-    console.log('Reading rows from the Table...');
+async function DeleteAllMsSQL(reset=true) {
+    await sql.connect(config);
+    console.log("connected");
 
-    // Read all rows from table
-    request = new Request(
-    'SELECT Id, Name, Location FROM TestSchema.Employees;',
-    function(err, rowCount, rows) {
-    if (err) {
-        callback(err);
+    if (reset) {
+        await sql.query`TRUNCATE TABLE ForwardTransaction.Contract`;
+        console.log("reseted");
     } else {
-        console.log(rowCount + ' row(s) returned');
-        callback(null);
+        await sql.query`DELETE FROM ForwardTransaction.Contract`;
+        console.log("deleted all data");
     }
-    });
-
-    // Print the rows read
-    var result = "";
-    request.on('row', function(columns) {
-        columns.forEach(function(column) {
-            if (column.value === null) {
-                console.log('NULL');
-            } else {
-                result += column.value + " ";
-            }
-        });
-        console.log(result);
-        result = "";
-    });
-
-    // Execute SQL statement
-    connection.execSql(request);
+    
+    sql.close();
 }
 
-function Complete(err, result) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Done!");
-    }
+async function main() {
+    let data = generateRandomObject();
+
+    await DeleteAllMsSQL();
+
+    await InsertOneMsSQL(data);
+
+    await SearchAllMsSQL();
+
+    await SearchOneMsSQL(1);
 }
 
-function closeConnection(err, result) {
-    connection.close();
-    console.log("Connection closed");
-}
-
-// Attempt to connect and execute queries if connection goes through
-connection.on('connect', function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Connected');
-
-    // Execute all functions in the array serially
-    async.waterfall([
-        Start,
-        Insert,
-        Update,
-        Delete,
-        Read,
-        closeConnection
-    ], Complete);
-  }
-
-});
-
+main();
